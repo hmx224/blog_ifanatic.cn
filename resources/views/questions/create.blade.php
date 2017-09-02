cd408769803c4bf39b33186f7da6a89b497339dd
humengxu <939631353@qq.com>
2017-09-02 11:58:49 +0800

    首页样式修改，字样问题改为文章

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">发布问题</div>
+                    <div class="panel-heading">发布文章</div>
                     <div class="panel-body">
                         <form action="/questions" method="POST">
 
@@ -44,7 +44,7 @@
                                 @endif
                             </div>
 
-                            <button class="btn btn-success pull-right" type="submit">发布问题</button>
+                            <button class="btn btn-success pull-right" type="submit">发布文章</button>
                         </form>
                     </div>
                 </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,25 +3,58 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                @foreach($questions as $question)
-                    <div class="media">
-                        <div class="media-left">
-                            <a href="">
-                                <img width="50px" src="{{ $question->user->avatar }}" alt="{{ $question->user->name }}">
-                            </a>
-                        </div>
+            <div class="col-md-8 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        @foreach($questions as $question)
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="">
+                                        <img width="50px" src="{{ $question->user->avatar }}"
+                                             alt="{{ $question->user->name }}">
+                                    </a>
+                                </div>
 
-                        <div class="media-body">
-                            <h4 class="media-heading">
-                                <a href="/questions/{{ $question->id }}">
-                                    {{ $question->title }}
-                                </a>
-                            </h4>
-                        </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading">
+                                        <a href="/questions/{{ $question->id }}">
+                                            {{ $question->title }}
+                                        </a>
+                                    </h4>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                @endforeach
+                </div>
             </div>
+
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading question-follow">
+                        <h2>
+                            {{ config('app.name', 'ifanatic.cn') }}
+                        </h2>
+                        <span>欢迎来到追逐血性的博客，分享你的所见所谓，让我们开怀聊天。</span>
+                    </div>
+
+                    <div class="panel-body">
+                        <a href="" class="btn btn-default pull-left">关于我</a>
+                        <a href="questions/create" class="btn btn-primary pull-right">写文章</a>
+                    </div>
+
+                </div>
+            </div>
+
+            {{--<div class="col-md-8 col-md-offset-1">--}}
+                {{--<div class="panel panel-default">--}}
+                    {{--<div class="panel-heading">--}}
+                        {{--测试面板--}}
+                    {{--</div>--}}
+                    {{--<div class="panel-body">--}}
+                        {{--测试内容--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
         </div>
     </div>
 @endsection
