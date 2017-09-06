566ec881c2b48bb2c97679600af6c8ae2e410fd5
humengxu <939631353@qq.com>
2017-09-06 10:34:37 +0800

    加上日志

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -25,13 +25,12 @@ class FileController extends BaseController
         $day = Carbon::now()->format('d');
         $time = Carbon::now()->format('YmdHis');
 
-        \Log::info('自定义image_path之前1', [config('site.upload.image_path')]);
-        \Log::info('自定义image_path之前2', [config('site["upload"]["image_path"]')]);
+        \Log::info('自定义image_path之前', [config('site.upload.image_path')]);
 
         //使用Config就use Config,用法： Config::key()，这里用config
-        $image_path = '/uploads/images';
+//        $image_path = '/uploads/images';
 
-        $relativePath = config('site.upload.image_path', $image_path) . '/' . $year . '/' . $month . $day . '/';
+        $relativePath = config('site.upload.image_path') . '/' . $year . '/' . $month . $day . '/';
 
         $uploadPath = public_path() . $relativePath;
 
