bfe542d156923c9a534eefe6f8070bfcffa5bed4
humengxu <939631353@qq.com>
2017-09-06 13:21:29 +0800

    底部固定样式

--- resources/views/widgets/footer.blade.php
+++ resources/views/widgets/footer.blade.php
@@ -1,7 +1,12 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 1.0.0
+<nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="navbar-inner navbar-content-center">
+        <div class="text-muted credit" style="padding: 14px;">
+        <div class="container">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 1.0.0
+            </div>
+            <strong>&copy; 2017-2018 <a href="http://blog.ifanatic.cn">爱狂热</a></strong> xiaoHu版权所有.
+        </div>
+        </div>
     </div>
-    <strong>&copy;&copy; 2017-2018 <a href="http://blog.ifanatic.cn">爱狂热</a></strong> xiaoHu版权所有.
-</footer>
-
+</nav>
