bc0eccec93342b0a81f97fb94a1d385bcef347b1
humengxu <939631353@qq.com>
2017-12-13 11:06:36 +0800

    加入框架统计代码

--- resources/views/widgets/footer.blade.php
+++ resources/views/widgets/footer.blade.php
@@ -25,107 +25,106 @@
 {{--</div>--}}
 {{--</div>--}}
 
-    {{--<style>--}}
-        {{--#footer ul li{--}}
-            {{--line-height: 1.9em;--}}
-        {{--}--}}
-        {{--#footer a{--}}
-            {{--color: #8e8e8e;--}}
-            {{--padding: .2em 0 0;--}}
-            {{--display: block;--}}
-            {{--border-radius: 4px;--}}
-        {{--}--}}
+{{--<style>--}}
+{{--#footer ul li{--}}
+{{--line-height: 1.9em;--}}
+{{--}--}}
+{{--#footer a{--}}
+{{--color: #8e8e8e;--}}
+{{--padding: .2em 0 0;--}}
+{{--display: block;--}}
+{{--border-radius: 4px;--}}
+{{--}--}}
 
-    {{--</style>--}}
+{{--</style>--}}
 
 {{--<div class="container" id="footer">--}}
-    {{--<div class="row">--}}
-        {{--<div class="col-md-4">--}}
-            {{--<h4 class="logo" id="site-description">Ifanatic</h4>--}}
-            {{--<p class="footer__site-description">--}}
-                {{--<em>I</em>fanatic <br>--}}
-                {{--<strong>为 Web 开发者而生</strong>,<strong>codecasts</strong> 的愿景是做最好的 PHP <a target="_blank"--}}
-                                                                                        {{--rel="noreferrer noopener"--}}
-                                                                                        {{--href="http://laravel.com/">Laravel </a>Vue.js--}}
-                {{--中文视频教程和问答社区,--}}
-                {{--有任何跟PHP <a target="_blank" rel="noreferrer noopener" href="http://laravel.com/">Laravel </a>Vue.js--}}
-                {{--相关的问题，都可以在这里得到解决。<strong>Happy Hacking !</strong>--}}
-            {{--</p>--}}
+{{--<div class="row">--}}
+{{--<div class="col-md-4">--}}
+{{--<h4 class="logo" id="site-description">Ifanatic</h4>--}}
+{{--<p class="footer__site-description">--}}
+{{--<em>I</em>fanatic <br>--}}
+{{--<strong>为 Web 开发者而生</strong>,<strong>codecasts</strong> 的愿景是做最好的 PHP <a target="_blank"--}}
+{{--rel="noreferrer noopener"--}}
+{{--href="http://laravel.com/">Laravel </a>Vue.js--}}
+{{--中文视频教程和问答社区,--}}
+{{--有任何跟PHP <a target="_blank" rel="noreferrer noopener" href="http://laravel.com/">Laravel </a>Vue.js--}}
+{{--相关的问题，都可以在这里得到解决。<strong>Happy Hacking !</strong>--}}
+{{--</p>--}}
 
 
+{{--<ul class="footer__connect">--}}
+{{--<li class="github pull-left" style="width:40px;height:40px;color: #8e8e8e;">--}}
+{{--<a target="_blank" rel="noreferrer noopener" href="https://github.com/hmx224" style="text-align: center">--}}
+{{--<i class="fa fa-github"></i>--}}
+{{--</a>--}}
+{{--</li>--}}
 
-            {{--<ul class="footer__connect">--}}
-                {{--<li class="github pull-left" style="width:40px;height:40px;color: #8e8e8e;">--}}
-                    {{--<a target="_blank" rel="noreferrer noopener" href="https://github.com/hmx224" style="text-align: center">--}}
-                        {{--<i class="fa fa-github"></i>--}}
-                    {{--</a>--}}
-                {{--</li>--}}
+{{--<li class="weibo pull-left"><a target="_blank" rel="noreferrer noopener" href="https://weibo.com/u/1666463282"><i--}}
+{{--class="fa fa-weibo"></i></a></li>--}}
 
-                {{--<li class="weibo pull-left"><a target="_blank" rel="noreferrer noopener" href="https://weibo.com/u/1666463282"><i--}}
-                                {{--class="fa fa-weibo"></i></a></li>--}}
-
-                {{--<li class="weixin">--}}
-                    {{--<a href="#"><i class="fa fa-weixin"></i></a>--}}
-                    {{--<div class="weixin-qr-code">--}}
-                        {{--<img src="https://jellybool.b0.upaiyun.com/qrcode_for_wechat_430.jpg"--}}
-                             {{--alt="JellyBool 微信公众号" width="160">--}}
-                    {{--</div>--}}
-                {{--</li>--}}
-
-
-                {{--<li class="qq">--}}
-                    {{--<a href="#"><i class="fa fa-qq"></i></a>--}}
-                    {{--<div class="qq-qr-code">--}}
-                        {{--<img src="/images/qq-group-3.jpg?version=2" alt="laravist QQ 群" width="120">--}}
-                    {{--</div>--}}
-                {{--</li>--}}
+{{--<li class="weixin">--}}
+{{--<a href="#"><i class="fa fa-weixin"></i></a>--}}
+{{--<div class="weixin-qr-code">--}}
+{{--<img src="https://jellybool.b0.upaiyun.com/qrcode_for_wechat_430.jpg"--}}
+{{--alt="JellyBool 微信公众号" width="160">--}}
+{{--</div>--}}
+{{--</li>--}}
 
 
-            {{--</ul>--}}
-        {{--</div>--}}
-        {{--<div class="col-md-2 not-mobile not-pad">--}}
-            {{--<h5>学习资料</h5>--}}
-            {{--<ul class="zeroed">--}}
-                {{--<li><a rel="noreferrer noopener" href="https://blog.ifanatic.cn">文章教程</a></li>--}}
-                {{--<li><a target="_blank" rel="noreferrer noopener" href="https://jellybool.com">个人博客</a></li>--}}
-                {{--<li><a href="https://www.codecasts.com/recommended-reading">推荐书目</a></li>--}}
+{{--<li class="qq">--}}
+{{--<a href="#"><i class="fa fa-qq"></i></a>--}}
+{{--<div class="qq-qr-code">--}}
+{{--<img src="/images/qq-group-3.jpg?version=2" alt="laravist QQ 群" width="120">--}}
+{{--</div>--}}
+{{--</li>--}}
 
-                {{--<li>--}}
-                    {{--<a target="_blank"--}}
-                       {{--href="//shang.qq.com/wpa/qunwpa?idkey=31435d309d493dcf3eef7f06cdc697edffefccbda13c43df97e47ca38e797034">--}}
-                        {{--QQ群 595928952--}}
-                    {{--</a>--}}
-                {{--</li>--}}
-            {{--</ul>--}}
-        {{--</div>--}}
-        {{--<div class="col-md-2 not-mobile not-pad">--}}
-            {{--<h5>Laravel讨论</h5>--}}
-            {{--<ul class="zeroed">--}}
-                {{--<li><a href="https://www.codecasts.com/learn">学习路径</a></li>--}}
-                {{--<li><a href="https://www.codecasts.com/agreement" target="_blank" rel="noreferrer noopener">版权声明</a>--}}
-                {{--</li>--}}
 
-                {{--<li><a href="https://www.codecasts.com/agreement">社区规则</a></li>--}}
-                {{--<li><a href="https://easywechat.org/" target="_blank" rel="noreferrer noopener">EasyWechat</a></li>--}}
-            {{--</ul>--}}
-        {{--</div>--}}
-        {{--<div class="col-md-2 not-mobile not-pad">--}}
-            {{--<h5>友情相关</h5>--}}
-            {{--<div class="row">--}}
-                {{--<div class="col-md-10">--}}
-                    {{--<ul class="zeroed">--}}
-                        {{--<li><a target="_blank" rel="noreferrer noopener" href="https://www.codecasts.com/vip">VIP 会员</a>--}}
-                        {{--</li>--}}
-                        {{--<li><a target="_blank" rel="noreferrer noopener" href="https://www.codecasts.com/link">友情链接</a>--}}
-                        {{--</li>--}}
-                        {{--<li><a href="https://www.codecasts.com/coin">电子货币购买</a></li>--}}
-                        {{--<li><a href="https://github.com/laravist" target="_blank" rel="noreferrer noopener">Codecasts--}}
-                                {{--Github</a></li>--}}
-                    {{--</ul>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
+{{--</ul>--}}
+{{--</div>--}}
+{{--<div class="col-md-2 not-mobile not-pad">--}}
+{{--<h5>学习资料</h5>--}}
+{{--<ul class="zeroed">--}}
+{{--<li><a rel="noreferrer noopener" href="https://blog.ifanatic.cn">文章教程</a></li>--}}
+{{--<li><a target="_blank" rel="noreferrer noopener" href="https://jellybool.com">个人博客</a></li>--}}
+{{--<li><a href="https://www.codecasts.com/recommended-reading">推荐书目</a></li>--}}
+
+{{--<li>--}}
+{{--<a target="_blank"--}}
+{{--href="//shang.qq.com/wpa/qunwpa?idkey=31435d309d493dcf3eef7f06cdc697edffefccbda13c43df97e47ca38e797034">--}}
+{{--QQ群 595928952--}}
+{{--</a>--}}
+{{--</li>--}}
+{{--</ul>--}}
+{{--</div>--}}
+{{--<div class="col-md-2 not-mobile not-pad">--}}
+{{--<h5>Laravel讨论</h5>--}}
+{{--<ul class="zeroed">--}}
+{{--<li><a href="https://www.codecasts.com/learn">学习路径</a></li>--}}
+{{--<li><a href="https://www.codecasts.com/agreement" target="_blank" rel="noreferrer noopener">版权声明</a>--}}
+{{--</li>--}}
+
+{{--<li><a href="https://www.codecasts.com/agreement">社区规则</a></li>--}}
+{{--<li><a href="https://easywechat.org/" target="_blank" rel="noreferrer noopener">EasyWechat</a></li>--}}
+{{--</ul>--}}
+{{--</div>--}}
+{{--<div class="col-md-2 not-mobile not-pad">--}}
+{{--<h5>友情相关</h5>--}}
+{{--<div class="row">--}}
+{{--<div class="col-md-10">--}}
+{{--<ul class="zeroed">--}}
+{{--<li><a target="_blank" rel="noreferrer noopener" href="https://www.codecasts.com/vip">VIP 会员</a>--}}
+{{--</li>--}}
+{{--<li><a target="_blank" rel="noreferrer noopener" href="https://www.codecasts.com/link">友情链接</a>--}}
+{{--</li>--}}
+{{--<li><a href="https://www.codecasts.com/coin">电子货币购买</a></li>--}}
+{{--<li><a href="https://github.com/laravist" target="_blank" rel="noreferrer noopener">Codecasts--}}
+{{--Github</a></li>--}}
+{{--</ul>--}}
+{{--</div>--}}
+{{--</div>--}}
+{{--</div>--}}
+{{--</div>--}}
 {{--</div>--}}
 
 <div class="scroll-back-to-top-wrapper">
@@ -139,9 +138,12 @@
         <div class="text-muted credit" style="padding: 14px;">
             <div class="container">
                 <div class="pull-right hidden-xs">
-                     2017~2018 | ICP备案证书号:京ICP备16009734-2号
+                    2017~2018 | ICP备案证书号:京ICP备16009734-2号
                 </div>
-                <b>Version</b> 1.0.0 <strong>&copy; 2017-2018 <a href="http://blog.ifanatic.cn">爱狂热</a></strong> huMengXu版权所有.
+                <b>Version</b> 1.0.0 <strong>&copy; 2017-2018 <a href="http://blog.ifanatic.cn">爱狂热</a></strong>
+                huMengXu版权所有.
+                <script>var lainframe;</script>
+                <script language="javascript" type="text/javascript" src="//js.users.51.la/19358042.js"></script>
             </div>
         </div>
     </div>
