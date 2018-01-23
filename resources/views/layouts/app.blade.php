8cf3aa1a71238131ea5701fda8476f447edb29dc
humengxu <939631353@qq.com>
2018-01-23 10:27:08 +0800

    增加seo优化关键字

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,11 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="爱狂热,博客,娱乐,心情,技术,php,web,js,mysql,linux,redis,mongodb,ifanatic,爱狂热科技创意,有限公司">
+    <meta name="description" content="爱狂热博客系统,是一款可以由用户注册分享生活中所见所闻的博客,ifanatic是一款发展中的博客系统">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -22,7 +25,8 @@
     </script>
 
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+{{--    <title>{{ config('app.name', 'Laravel') }}</title>--}}
+    <title>爱狂热博客平台</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -37,16 +41,21 @@
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/plugins/font-awesome/4.7.0/css/font-awesome.css">
-    <!--layui-->
 
-{{--<link href="/plugins/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">--}}
-{{--<script src="/plugins/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
+    <!-- layui -->
 
-<!--X-editable-->
-{{--<link href="/plugins/x-editable/1.5.1/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet">--}}
-{{--<script src="/plugins/x-editable/1.5.1/bootstrap3-editable/js/bootstrap-editable.min.js"></script>--}}
+    {{--<link rel="stylesheet" href="/plugins/layui/2.1.2/css/layui.css" media="all">--}}
+    {{--<script src="/plugins/layui/2.1.2/layui.js" charset="utf-8"></script>--}}
 
-<!--Moment.js-->
+    <!--bootstrap-->
+    {{--<link href="/plugins/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">--}}
+    {{--<script src="/plugins/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
+
+    <!--X-editable-->
+    {{--<link href="/plugins/x-editable/1.5.1/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet">--}}
+    {{--<script src="/plugins/x-editable/1.5.1/bootstrap3-editable/js/bootstrap-editable.min.js"></script>--}}
+
+    <!--Moment.js-->
     {{--<script src="/plugins/moment.js/2.15.1/moment.min.js"></script>--}}
     {{--<script src="/plugins/moment.js/2.15.1/moment-with-locales.min.js"></script>--}}
     {{--<script src="/plugins/moment.js/2.15.1/locales.js"></script>--}}
@@ -57,7 +66,8 @@
 
 
 </head>
-<body style="padding:60px 0 60px 0">
+<body style="padding:100px 0 80px 0">
+
 <div id="app">
     @include('widgets.header')
 
@@ -71,12 +81,12 @@
                 {!! session('flash_notification.message') !!}
             </div>
         @endif
+
     </div>
-
     @yield('content')
-
     @include('widgets.footer')
 </div>
+
 
 @yield('js')
 
